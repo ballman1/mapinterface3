754f10daf2454d1c0c6c86c8576ec10964100427
Marc Weinstein <marcweinstein@Marcs-Mac-mini.local>
2025-06-22 20:20:05 -0400

    Initial commit

---
+++ public/epimap/public/epimap/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAEqp-BO5SGdoKEB154FfgVsRP0cdaxpAU",
+  authDomain: "epipen-finder-map-34982.firebaseapp.com",
+  projectId: "epipen-finder-map-34982",
+  storageBucket: "epipen-finder-map-34982.firebasestorage.app",
+  messagingSenderId: "389714610819",
+  appId: "1:389714610819:web:a1a6310b98039d9ab2e9b6",
+  measurementId: "G-KZ5PPMNKEQ"
+};
